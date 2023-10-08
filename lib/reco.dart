b26f65c78db1f2b56fcd897566d41ee37cfb356f
juanFFG <juanfelipefernandez881@gmail.com>
2023-10-07 22:05:45 -0500

    Feat: Recomendacion terminada

--- lib/reco.dart
+++ lib/reco.dart
@@ -1,16 +1,172 @@
 import 'package:flutter/material.dart';
 
-
 class Reco extends StatelessWidget {
   const Reco({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      backgroundColor: const Color(0xFF6F73D2),
       appBar: AppBar(
         title: const Text("Recomendación"),
         backgroundColor: const Color(0xffEF476F),
         toolbarHeight: 40,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 250,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              color: Color(0xff52d1dc),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 125,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                color: Color(0xffEF476F),
+              ),
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  child: Image.asset(
+                    'assets/gtav.jpg',
+                    fit: BoxFit.cover,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Center(
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Center(
+                    child: Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 250,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              color: Color(0xffEF476F),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 125,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                color: Color(0xff52d1dc),
+              ),
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  child: Image.asset(
+                    'assets/terraria.jpg',
+                    fit: BoxFit.cover,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Center(
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Center(
+                    child: Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
